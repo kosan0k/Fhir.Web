85e9ee4f8627bee80bfac49c28ef787200302d94
Alexander Zhishkevich <kosan0k230@gmail.com>
2024-12-25 12:36:33 +0300

    +: patient generator util

---
+++ Utils/Fhir.Patient.Utils.EntityGenerator/PatientGenerator.cs
@@ -0,0 +1,65 @@
+﻿using Fhir.Patients.Domain.Models;
+
+namespace Fhir.Patient.Utils.EntityGenerator
+{
+    internal static class PatientGenerator
+    {
+        private static readonly Random _random = new();
+
+        private static readonly List<string> _firstNames =
+        [
+            "John", "Jane", "Michael", "Emily", "William", "Sophia",
+            "James", "Isabella", "Robert", "Olivia", "David", "Emma"
+        ];
+
+        private static readonly List<string> _middleNames =
+        [
+            "Alexander", "Grace", "Marie", "Elizabeth", "Joseph", "Andrew",
+            "Charlotte", "Rose", "Liam", "Noah", "Benjamin", "Lucas"
+        ];
+
+        private static readonly List<string> _lastNames =
+        [
+            "Smith", "Johnson", "Williams", "Brown", "Jones", "Garcia",
+            "Miller", "Davis", "Rodriguez", "Martinez", "Hernandez", "Lopez"
+        ];
+
+        internal static Patients.Domain.Models.Patient GenerateRandomPatient()
+        {
+            return new Patients.Domain.Models.Patient
+            {
+                Name = new Name
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Use = "official",
+                    Family = GetRandomElement(_lastNames),
+                    Given = 
+                    [
+                        GetRandomElement(_firstNames),
+                        GetRandomElement(_middleNames)
+                    ]
+                },
+                Gender = GenerateRandomGender(),
+                BirthDate = GenerateRandomDate(DateTime.Now.AddDays(-14), DateTime.Now),
+                Active = _random.Next(0, 2) == 1
+            };
+        }
+
+        private static string GetRandomElement(List<string> list)
+        {
+            return list[_random.Next(list.Count)];
+        }
+
+        private static Gender GenerateRandomGender()
+        {
+            var genders = Enum.GetValues<Gender>();
+            return (Gender)genders.GetValue(_random.Next(genders.Length))!;
+        }
+
+        private static DateTime GenerateRandomDate(DateTime start, DateTime end)
+        {
+            int range = (end - start).Days;
+            return start.AddDays(_random.Next(range)).AddSeconds(_random.Next(0, 86400));
+        }
+    }
+}
---
+++ Utils/Fhir.Patient.Utils.EntityGenerator/Program.cs
@@ -0,0 +1,88 @@
+﻿using System.Text.Json;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace Fhir.Patient.Utils.EntityGenerator;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Started");
+
+        var apiUrl = args.Length != 0
+            ? args[0]
+            : "http://localhost:5076/patients";
+
+        using HttpClient httpClient = new();
+
+        if(await IsServiceAvailable(httpClient, apiUrl))
+            await SendRandomPatientsAsync(httpClient, apiUrl);
+        else
+            Console.WriteLine("Service is unavailable after multiple attempts.");
+
+        Console.WriteLine("Done");
+        Console.ReadLine();
+    }
+
+    private static async Task SendRandomPatientsAsync(HttpClient httpClient, string apiUrl)
+    {
+        var serializationOptions = new JsonSerializerOptions { WriteIndented = true };
+        serializationOptions.Converters.Add(new JsonStringEnumConverter(JsonNamingPolicy.CamelCase));
+
+        for (int i = 0; i < 100; i++)
+        {
+            var patient = PatientGenerator.GenerateRandomPatient();
+
+            string patientJson = JsonSerializer.Serialize(patient, serializationOptions);
+            StringContent content = new(patientJson, Encoding.UTF8, "application/json");
+
+            try
+            {
+                HttpResponseMessage response = await httpClient.PostAsync(apiUrl, content);
+
+                if (response.IsSuccessStatusCode)
+                    Console.WriteLine($"Patient sent successfully");
+                else
+                    Console.WriteLine($"Failed to send patient. Status Code: {response.StatusCode}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error sending patient: {ex.Message}");
+            }
+        }
+    }
+
+    private static async Task<bool> IsServiceAvailable(HttpClient httpClient, string apiUrl)
+    {
+        const int maxRetries = 5;
+        int retryDelayMs = 2000;
+
+        for (int attempt = 1; attempt <= maxRetries; attempt++)
+        {
+            try
+            {
+                Console.WriteLine($"Pinging service (attempt {attempt})...");
+                HttpResponseMessage response = await httpClient.GetAsync(apiUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("Service is available.");
+                    return true;
+                }
+            }
+            catch
+            {
+                // Ignoring exceptions during ping.
+            }
+
+            Console.WriteLine($"Service unavailable. Retrying in {retryDelayMs / 1000} seconds...");
+            await Task.Delay(retryDelayMs);
+
+            retryDelayMs *= 2;
+        }
+
+        return false;
+    }
+}
+
